2adee82bf78f7a7bae1a5d3a677f28a626da0941
liao_mo <b10815026@gapps.ntust.edu.tw>
2023-10-16 17:20:50 +0800

    developing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,61 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
+import Cookies from "js-cookie";
 
 function App() {
   const [isLogin, setIsLogin] = useState(false);
   const [userData, setUserData] = useState(undefined);
+
+  useEffect(function () {
+    const token = Cookies.get("jwt");
+    async function authenticate() {
+      let config = {
+        method: "get",
+        maxBodyLength: Infinity,
+        url: "http://localhost:3000/users/me",
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTJjZWM3OGFhOTIzMWRkODQ4NzE1OWEiLCJpYXQiOjE2OTc0NDY3NTh9.eA_aQ1ZO_zRGhi5cthDnjwuljWGVa0IR8ivbF9LC2vM",
+        },
+      };
+
+      try {
+        const res = await axios.request(config);
+        // console.log(res);
+
+        //sucessfully authenticate
+        if (res.status === 200) {
+          setIsLogin(true);
+          setUserData(res.data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    if (token) {
+      authenticate();
+    }
+  }, []);
+
   function handleLogin(data) {
     setIsLogin(true);
     setUserData(data);
   }
+
+  function handleLogout() {
+    setIsLogin(false);
+    setUserData(undefined);
+    Cookies.remove("jwt");
+  }
+
   return (
     <div className="App">
       {!isLogin ? (
         <Login onLogin={handleLogin} />
       ) : (
-        <ControlPanel userData={userData} />
+        <ControlPanel userData={userData} onLogout={handleLogout} />
       )}
     </div>
   );
@@ -48,9 +89,13 @@ function Login({ onLogin }) {
         //sucessfully login
         if (res.status === 200) {
           onLogin(res.data.user);
+          // console.log(res.data);
+          Cookies.set("jwt", res.data.token, { expires: 7 });
+          // console.log(Cookies.get("jwt"));
         }
       } catch (error) {
         console.log(error);
+        setIsFailed(true);
       }
     }
 
@@ -73,26 +118,39 @@ function Login({ onLogin }) {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button className="btn-login" onClick={handleLogin}>
-          Login
-        </button>
+        <div className="login-hint">
+          {isFailed && <p className="failed-text">Failed to login</p>}
+          <button className="btn-login" onClick={handleLogin}>
+            Login
+          </button>
+        </div>
       </div>
     </div>
   );
 }
 
-function ControlPanel({ userData }) {
+function ControlPanel({ userData, onLogout }) {
+  const [functionality, setFunctionality] = useState("profile");
   return (
     <div>
-      <h2>Control Panel</h2>
-      <UserProfile userData={userData} />
+      <header className="control-header">
+        <h2>Control Panel</h2>
+        <button className="btn-logout" onClick={onLogout}>
+          Logout
+        </button>
+      </header>
+      <button onClick={() => setFunctionality("profile")}>view profile</button>
+      <button onClick={() => setFunctionality("update")}>update profile</button>
+
+      {functionality === "profile" && <UserProfile userData={userData} />}
+      {functionality === "update" && <p>update</p>}
     </div>
   );
 }
 
 function UserProfile({ userData }) {
   return (
-    <div>
+    <div className="user-profile">
       <p>Your Name: {userData.name}</p>
       <p>Your age: {userData.age}</p>
       <p>Your email: {userData.email}</p>
