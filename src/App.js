90717a9cc7c1b39ae875cb14cd9087fcef493566
MOCHE, LIAO <pppoooppp654852@gmail.com>
2023-10-16 09:19:55 +0800

    Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [isLogin, setIsLogin] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!isLogin ? <Login onLogin={setIsLogin} /> : <ControlPanel />}
+    </div>
+  );
+}
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleLogin(onLogin) {
+    async function loginUser() {
+      try {
+        let data = JSON.stringify({
+          email,
+          password,
+        });
+
+        let config = {
+          method: "post",
+          maxBodyLength: Infinity,
+          url: "http://localhost:3000/users/login",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          data: data,
+        };
+
+        axios
+          .request(config)
+          .then((response) => {
+            console.log(response);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    loginUser();
+  }
+
+  return (
+    <div className="login-box">
+      <h1>Login</h1>
+      <div className="input-grid">
+        <label>Email</label>
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label>Password</label>
+        <input
+          type="text"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className="btn-login" onClick={handleLogin}>
+          Login
+        </button>
+      </div>
+    </div>
+  );
+}
+
+function ControlPanel() {
+  return (
+    <div>
+      <h2>Control Panel</h2>
     </div>
   );
 }
